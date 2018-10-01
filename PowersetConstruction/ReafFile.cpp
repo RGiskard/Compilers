6a9cd71ac6f7ac28795dcb936b337c2ccbd50104
RGiskard <rcondorib@gmail.com>
2018-09-30 19:56:39 -0500

    set union, read files

--- PowersetConstruction/ReafFile.cpp
+++ PowersetConstruction/ReafFile.cpp
@@ -1,10 +1,36 @@
-#include <iostream>
 #include <fstream>
-#include <string>
-#include <iterator>
-#include <vector>
 #include <sstream>
+#include <iostream>
+#include <tuple>
+#include <string>
+#include <vector>
+#include <stack>
+#include <map>
+#include <algorithm>
+#include <set>
+
 using namespace std;
+
+typedef map<int,map<int,int>> Tabela;
+typedef map<int,map<int,set<int>>> TabelaState;
+/*Imprimir conjuntos*/
+template<typename T>
+void printSet(set<T> conjunto)
+{
+	cout<<"{";
+	for(auto it=conjunto.begin();it!=conjunto.end();it++)
+		cout<<*it<<",";
+	cout<<"}";
+}	
+
+/*Union de conjuntos*/
+template<typename T>
+set<T> U(set<T> A,set<T> B){
+	set<T> unir(A);
+	for(auto it=B.begin();it!=B.end();it++)
+		unir.insert(*it);
+	return unir;
+}	
 
 vector<string> &split(const string &s, char delim,vector<string> &elems) {
 	stringstream ss(s);
@@ -18,7 +44,7 @@ vector<string> &split(const string &s, char delim,vector<string> &elems) {
 vector<int> split(const string &s, char delim) {
 	vector<string> elems;
 	split(s, delim, elems);
-	// Fill DataNumbers
+	// llenado  DataNumbers
 	vector<int> intNumbers;
 	for (unsigned int i=0; i<elems.size(); i++)
 	{
@@ -53,11 +79,29 @@ int main () {
 		int totalEntradas=atoi(line.c_str());
 		getline (myfile,line);//Total Entradas
 		vector<int> Entradas=split(line,' ');
-		for(auto it=Entradas.begin();it!=Entradas.end();it++)
-			cout<<*it<<" ";
-		cout<<endl << line<< '\n';	
-		//}
-		//myfile.close();
+		getline (myfile,line);//Cuarta parte del contenido a no considerar;
+		getline (myfile,line);
+		int totalTransiciones=atoi(line.c_str());
+		//cout<<endl << ++totalTransiciones<< '\n';	
+		//LLenado Tablas de Transicion y Tabla de estados
+		vector<int> tempTransition;
+		Tabela trans;
+		TabelaState tranState;
+		for(int i=0;i<totalTransiciones;i++)
+		{
+			getline (myfile,line);
+			tempTransition=split(line,' ');
+			//LLenado Tabla de transicion
+			trans[tempTransition[0]][tempTransition[2]]=tempTransition[1];
+			(tranState[tempTransition[0]][tempTransition[1]]).insert(tempTransition[2]);
+			//cout<<line<<endl;
+		}
+		for(auto it=trans.begin();it!=trans.end();it++);
+		//cout<<tranState.size()<<endl;
+		
+
+		printSet(U(tranState[3][-1],tranState[6][-1]));
+		myfile.close();
 	}
 	else cout << "Unable to open file"; 
 	
