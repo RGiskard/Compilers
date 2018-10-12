95755915e916957862de165bcb80d38c6a3bb4af
RGiskard <rcondorib@gmail.com>
2018-10-11 19:05:45 -0500

    declaración de estructuras

--- Calculator/calculator.cpp
+++ Calculator/calculator.cpp
@@ -12,8 +12,29 @@ int error()
 	fprintf(stderr,"Error\n");
 	exit(1);
 }
+
+void match(char expectedToken)
+{
+	if(token==expectedToken)
+		token=getchar();
+	else
+		error();
+}
+
 int main(int argc, char *argv[]) {
 	
+	int result;
+	token=getchar();
+	result=exp();
+	if(token=='\n')
+		printf("Result =%d\n",result);
+	else
+		error();
 	return 0;
 }
 
+int exp()
+{
+	int temp=0;
+	return temp;
+}
