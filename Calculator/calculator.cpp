6a863d38c53f7db32c5a51ad2c87d250588cc980
RGiskard <rcondorib@gmail.com>
2018-10-11 19:25:46 -0500

    Final

--- Calculator/calculator.cpp
+++ Calculator/calculator.cpp
@@ -35,6 +35,47 @@ int main(int argc, char *argv[]) {
 
 int exp()
 {
-	int temp=0;
+	int temp=term();
+	while((token=='+')||(token=='-'))
+	{
+		switch(token){
+		case '+': match('+');
+			temp+=term();
+			break;
+		case '-': match('-');
+			temp-=term();
+			break;
+		}
+	}
+	return temp;
+}
+
+int term()
+{
+	int temp=factor();
+	while(token=='*')
+	{
+		match('*');
+		temp*=factor();
+	}
+	return temp;
+}
+
+int factor()
+{
+	int temp;
+	if(token=='(')
+	{
+		match('(');
+		temp=exp();
+		match(')');
+	}
+	else if(isdigit(token))
+	{
+		ungetc(token,stdin);
+		scanf("%d",&temp);
+		token=getchar();
+	}
+	else error();
 	return temp;
 }
