ccf5994e7a20e2556128a85ab06e3a2dabf79939
RGiskard <rcondorib@gmail.com>
2018-11-10 23:12:08 -0500

    Aritmetica y boolean final

--- Flex and Bison/flex/ScannerBoolean.cpp
+++ Flex and Bison/flex/ScannerBoolean.cpp
@@ -314,8 +314,8 @@ int yyFlexLexer::yywrap() { return 1; }
 	(yy_hold_char) = *yy_cp; \
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
-#define YY_NUM_RULES 8
-#define YY_END_OF_BUFFER 9
+#define YY_NUM_RULES 10
+#define YY_END_OF_BUFFER 11
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -323,10 +323,10 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[14] =
+static const flex_int16_t yy_accept[16] =
     {   0,
-        0,    0,    9,    7,    6,    8,    3,    4,    1,    2,
-        5,    2,    0
+        0,    0,   11,    9,    8,   10,    3,    4,    6,    7,
+        1,    2,    5,    2,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
@@ -335,23 +335,23 @@ static const YY_CHAR yy_ec[256] =
         1,    2,    2,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    2,    1,    2,    1,    1,    1,    1,    1,    4,
-        5,    1,    1,    1,    1,    1,    1,    6,    6,    7,
-        7,    7,    7,    7,    7,    7,    7,    1,    1,    1,
-        1,    1,    1,    1,    8,    8,    8,    8,    8,    8,
-        8,    8,    8,    8,    8,    8,    8,    8,    8,    8,
-        8,    8,    8,    8,    8,    8,    8,    8,    8,    8,
-        1,    1,    1,    1,    9,    1,    8,    8,    8,    8,
+        5,    6,    7,    1,    1,    1,    1,    8,    8,    9,
+        9,    9,    9,    9,    9,    9,    9,    1,    1,    1,
+        1,    1,    1,    1,   10,   10,   10,   10,   10,   10,
+       10,   10,   10,   10,   10,   10,   10,   10,   10,   10,
+       10,   10,   10,   10,   10,   10,   10,   10,   10,   10,
+        1,    1,    1,    1,   11,    1,   10,   10,   10,   10,
 
-        8,    8,    8,    8,    8,    8,    8,    8,    8,    8,
-        8,    8,    8,    8,    8,    8,    8,    8,    8,    8,
-        8,    8,    1,    1,    1,   10,    1,    1,    1,    1,
+       10,   10,   10,   10,   10,   10,   10,   10,   10,   10,
+       10,   10,   10,   10,   10,   10,   10,   10,   10,   10,
+       10,   10,    1,    1,    1,   12,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    8,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,   10,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    8,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,   10,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    8,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,   10,    1,    1,    1,    1,    1,
 
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -361,35 +361,36 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static const YY_CHAR yy_meta[11] =
-    {   0,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1
-    } ;
-
-static const flex_int16_t yy_base[14] =
-    {   0,
-        0,    0,   15,   16,   16,   16,   16,   16,   16,    5,
-       16,    0,   16
-    } ;
-
-static const flex_int16_t yy_def[14] =
-    {   0,
-       13,    1,   13,   13,   13,   13,   13,   13,   13,   13,
-       13,   10,    0
-    } ;
-
-static const flex_int16_t yy_nxt[27] =
-    {   0,
-        4,    5,    6,    7,    8,    9,    4,   10,    4,   11,
-       12,   12,   12,   12,   13,    3,   13,   13,   13,   13,
-       13,   13,   13,   13,   13,   13
-    } ;
-
-static const flex_int16_t yy_chk[27] =
+static const YY_CHAR yy_meta[13] =
     {   0,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-       10,   10,   10,   10,    3,   13,   13,   13,   13,   13,
-       13,   13,   13,   13,   13,   13
+        1,    1
+    } ;
+
+static const flex_int16_t yy_base[16] =
+    {   0,
+        0,    0,   17,   18,   18,   18,   18,   18,   18,   18,
+       18,    5,   18,    0,   18
+    } ;
+
+static const flex_int16_t yy_def[16] =
+    {   0,
+       15,    1,   15,   15,   15,   15,   15,   15,   15,   15,
+       15,   15,   15,   12,    0
+    } ;
+
+static const flex_int16_t yy_nxt[31] =
+    {   0,
+        4,    5,    6,    7,    8,    9,   10,   11,    4,   12,
+        4,   13,   14,   14,   14,   14,   15,    3,   15,   15,
+       15,   15,   15,   15,   15,   15,   15,   15,   15,   15
+    } ;
+
+static const flex_int16_t yy_chk[31] =
+    {   0,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,   12,   12,   12,   12,    3,   15,   15,   15,
+       15,   15,   15,   15,   15,   15,   15,   15,   15,   15
     } ;
 
 /* The intent behind this definition is that it'll catch
@@ -403,8 +404,8 @@ static const flex_int16_t yy_chk[27] =
 #line 2 "lexicoBoolean.l"
     #include <stdio.h>
     #include <stdlib.h>
-#line 407 "ScannerBoolean.cpp"
 #line 408 "ScannerBoolean.cpp"
+#line 409 "ScannerBoolean.cpp"
 
 #define INITIAL 0
 
@@ -536,10 +537,10 @@ YY_DECL
 		}
 
 	{
-#line 24 "lexicoBoolean.l"
+#line 26 "lexicoBoolean.l"
 
 
-#line 543 "ScannerBoolean.cpp"
+#line 544 "ScannerBoolean.cpp"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -566,13 +567,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 14 )
+				if ( yy_current_state >= 16 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 16 );
+		while ( yy_base[yy_current_state] != 18 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -598,45 +599,55 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 26 "lexicoBoolean.l"
+#line 28 "lexicoBoolean.l"
 {printf("Símbolo %s\n",yytext);}
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
-#line 27 "lexicoBoolean.l"
+#line 29 "lexicoBoolean.l"
 {printf("Identificador %s\n",yytext);}
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 28 "lexicoBoolean.l"
+#line 30 "lexicoBoolean.l"
 {printf("Parentesis izquierdo %s\n",yytext);}
 	YY_BREAK
 case 4:
 YY_RULE_SETUP
-#line 29 "lexicoBoolean.l"
+#line 31 "lexicoBoolean.l"
 {printf("Parentesis Derecho %s\n",yytext);}
 	YY_BREAK
 case 5:
 YY_RULE_SETUP
-#line 30 "lexicoBoolean.l"
+#line 32 "lexicoBoolean.l"
 {printf("Operador Negate %s\n",yytext);}
 	YY_BREAK
 case 6:
 YY_RULE_SETUP
-#line 31 "lexicoBoolean.l"
-{}
+#line 33 "lexicoBoolean.l"
+{printf("Operador conjunction %s\n",yytext);}
 	YY_BREAK
 case 7:
 YY_RULE_SETUP
-#line 32 "lexicoBoolean.l"
-{printf("ERROR LEXICO\n");}
+#line 34 "lexicoBoolean.l"
+{printf("Operador disjunction %s\n",yytext);}
 	YY_BREAK
 case 8:
 YY_RULE_SETUP
 #line 35 "lexicoBoolean.l"
+{}
+	YY_BREAK
+case 9:
+YY_RULE_SETUP
+#line 36 "lexicoBoolean.l"
+{printf("ERROR LEXICO\n");}
+	YY_BREAK
+case 10:
+YY_RULE_SETUP
+#line 39 "lexicoBoolean.l"
 ECHO;
 	YY_BREAK
-#line 640 "ScannerBoolean.cpp"
+#line 651 "ScannerBoolean.cpp"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1053,7 +1064,7 @@ int yyFlexLexer::yy_get_next_buffer()
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 14 )
+			if ( yy_current_state >= 16 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1081,11 +1092,11 @@ int yyFlexLexer::yy_get_next_buffer()
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 14 )
+		if ( yy_current_state >= 16 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 13);
+	yy_is_jam = (yy_current_state == 15);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1599,6 +1610,6 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 35 "lexicoBoolean.l"
+#line 39 "lexicoBoolean.l"
 
 
